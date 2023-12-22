85922abad1bdeff2f48826cc2870d8bcef14b891
Patience Otuke <patienceotuke@gmail.com>
2023-12-22 22:41:31 +0300

    Requirment updates

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create school file
 
 file {'/tmp/school':
-    path    => '/tmp/school'
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+  path    => '/tmp/school'
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
