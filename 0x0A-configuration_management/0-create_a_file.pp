ee48ed24a5934d13f761123d327b5659a63e805e
Patience Otuke <patienceotuke@gmail.com>
2023-12-22 22:16:51 +0300

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
-file{'/tmp/school'
-path => '/tmp/school'
-owner => 'www-data'
-group => 'www-data'
-mode => '0744'
-content => 'I love Puppet'
+file{'/tmp/school':
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
