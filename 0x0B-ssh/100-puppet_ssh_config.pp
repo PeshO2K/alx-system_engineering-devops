27844861a09db7e7291471880606bd69b8bd8a26
Patience Otuke <patienceotuke@gmail.com>
2024-01-04 15:12:02 +0300

    Advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,7 @@
-#!/usr/bin/env bash
+# config file f0r server
+
+
+file { '/etc/ssh/ssh_config':
+  ensure  => file,
+  content => "Host 18.204.14.158\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
+}
