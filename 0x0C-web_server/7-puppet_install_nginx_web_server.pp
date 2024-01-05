d65f23880c37b285ea41cc766bf26382923d0b3b
Patience Otuke <patienceotuke@gmail.com>
2024-01-05 19:14:35 +0300

    Puupet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,37 @@
+# Install nginx and configure it
+
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => "
+server {
+    listen 80;
+    server_name _;
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}",
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/nginx/sites-available/default'],
+  }
+}
+
+include nginx
