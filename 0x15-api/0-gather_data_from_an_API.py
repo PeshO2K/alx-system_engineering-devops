0bcba34e41bd381a2e44cccc69e54c9c69be4b5f
PeshO2K <patienceotuke@gmial.com>
2024-02-08 20:19:31 +0300

    pycodestyle fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -21,19 +21,20 @@ def gather_data(emp_id):
 
 
 def completed_tasks(data_tuple):
-        edata = data_tuple[0]
-        tdata = data_tuple[1]
-        emp_name = edata['name']
-        tcompleted = [task for task in tdata if task['completed'] is True]
-        # print(tcompleted)
-        tdone = len(tcompleted)
-        ttotal = len(tdata)
-        ttitles = "\n\t " + "\n\t ".join([task['title'] for task in tcompleted])
+    edata = data_tuple[0]
+    tdata = data_tuple[1]
+    emp_name = edata['name']
+    tcompleted = [task for task in tdata if task['completed'] is True]
+    # print(tcompleted)
+    tdone = len(tcompleted)
+    ttotal = len(tdata)
+    ttitles = "\n\t " + \
+        "\n\t ".join([task['title'] for task in tcompleted])
 
-        my_comp_tasks = "".join(
-            [f"Employee {emp_name} is done with tasks({tdone}/{ttotal}):",
-                ttitles])
-        print(my_comp_tasks)
+    my_comp_tasks = "".join(
+        [f"Employee {emp_name} is done with tasks({tdone}/{ttotal}):",
+            ttitles])
+    print(my_comp_tasks)
 
 
 if __name__ == "__main__":
